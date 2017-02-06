04c9e787a52890b9124c9db433987a99ce320a35
Piotrek Bronowski <bronowski@gmail.com>
2017-02-06 00:21:02 +0000

    Adding loparser adn siegemem template.

---
+++ logparser.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Piotrek Bronowski <piotr.bronowski@redembedded.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Parses provided log file according to rules given in template file and writes result file in desired format
+"""
+
+import textfsm
+import csv
+from optparse import OptionParser
+
+
+class Parser:
+    def __init__(self, options):
+        self.input = options
+        self.template = options
+        self.output = options
+        self.format = options
+        self.header = []
+        self.results = [list()]
+
+    def write(self):
+        if self.format == "csv":
+            with open(self.output, 'w') as csvfile:
+                csvWriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL, dialect=csv.excel, lineterminator='\n')
+                csvWriter.writerow(self.header)
+                for row in self.results:
+                    csvWriter.writerow(row)
+
+    def parse(self):
+        # Open the template file, and initialise a new TextFSM object with it.
+        fsm = textfsm.TextFSM(open(self.template))
+        self.results = fsm.ParseText(self.input)
+        self.header = fsm.header
+        self.write()
+
+if __name__ == '__main__':
+    options_parser = OptionParser()
+    options_parser.add_option("-i", "--input",
+                              action="store", type="string", dest="input", default="input.log",
+                              help="reads input to parse from FILE", metavar="FILE")
+    options_parser.add_option("-o", "--output",
+                              action="store", type="string", dest="output", default="output.txt",
+                              help="reads input to parse from FILE", metavar="FILE")
+    options_parser.add_option("-f", "--format",
+                              action="store", type="string", dest="format", choices=["csv", "jira", "nice"], default="csv",
+                              help="define output file FORMAT: csv, jira, nice [default: %default]", metavar="FORMAT")
+    options_parser.add_option("-t", "--template",
+                              action="store", type="string", dest="template", default="siegemem_template",
+                              help="reads parsing rules from template FILE", metavar="FILE")
+
+    options_parser.set_usage('usage: logparser.py [options]')
+    (options, args) = options_parser.parse_args()
+    try:
+        parser = Parser(options)
+        parser.parse()
+    except:
+        options_parser.print_help()
+        raise
